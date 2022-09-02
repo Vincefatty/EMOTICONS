cd3ac37ea747a371e185e75393e3e66763ae91a3
Vincent Salazar <100139821@lubbockisd.net>
2022-09-02 09:32:56 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("((-_-))");
+        System.out.println("(0_0)");
+        System.out.println("(^-^)");
+        System.out.println("(^-0)");
+        System.out.println("(^3^)-d");
+    }
+}
